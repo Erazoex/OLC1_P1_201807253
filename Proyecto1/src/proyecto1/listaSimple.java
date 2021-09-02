a4bb567941a3c68ad91c7b15c9e96d98ae7d8b87
Brian Erazo <3012750170101@ingenieria.usac.edu.gt>
2021-09-02 00:17:52 -0600

    extraccion de datos sintacticojs

---
+++ Proyecto1/src/proyecto1/listaSimple.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+/**
+ *
+ * @author brian
+ */
+public class listaSimple {
+    public nodo raiz;
+    public int tamano;
+    public nodo ultimo;
+    
+    public void listSimple(){
+        this.raiz = null;
+        this.ultimo = null;
+        this.tamano = 0;
+    }
+    
+    public boolean estaVacia(){
+        if(tamano == 0){
+            return true;
+        }
+        return false;
+    }
+    
+    public int getTamano(){
+        return tamano;
+    }
+    
+    public void insertarNodo(String Tipo, String id,int cantidadLineas, int cantidadParametros){
+        nodo new_nodo = new nodo();
+        new_nodo.id = id;
+        new_nodo.Tipo = Tipo;
+        new_nodo.cantidadLineas = cantidadLineas;
+        new_nodo.cantidadParametros = cantidadParametros;
+        
+        if(estaVacia()){
+            this.raiz = this.ultimo = new_nodo;
+            tamano++;
+        }else{
+            this.ultimo.siguiente = new_nodo;
+            this.ultimo = this.ultimo.siguiente;
+            tamano++;
+        }
+    }
+    
+    public void imprimirLista(){
+        if(estaVacia()){
+            System.out.println("La lista esta vacia");
+        }else{
+            nodo aux = this.raiz;
+            while(aux!= this.ultimo.siguiente){
+                System.out.println("nombre: "+aux.id);
+                System.out.println("tipo: " + aux.Tipo);
+                System.out.println("cantidadlineas: " + aux.cantidadLineas);
+                System.out.println("cantidadparams: " + aux.cantidadParametros);
+                System.out.println("");
+                System.out.println("");
+                aux = aux.siguiente;
+            }
+        }
+        
+    }
+    
+}
---
+++ Proyecto1/src/proyecto1/nodo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+/**
+ *
+ * @author brian
+ */
+public class nodo {
+    public String id;
+    public String Tipo;
+    public int cantidadLineas;
+    public int cantidadParametros;
+    public nodo siguiente;
+            
+    
+    public void nodo(String tipo, String id, int cantidadLineas, int cantidadParametros){
+        this.id = id;
+        this.Tipo = tipo;
+        this.cantidadLineas = cantidadLineas;
+        this.cantidadParametros = cantidadParametros;
+        this.siguiente = null;
+    }
+    
+    public void nodo(){
+        
+    }
+}
